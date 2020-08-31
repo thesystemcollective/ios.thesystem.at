3e3c753ec770db629bae16f5f281579f086dd5c8
jascha <jascha@jaeh.at>
2020-08-31 21:43:05 +0200

    remove sourcemap files

--- docs/js/HemisphereLight.js
+++ docs/js/HemisphereLight.js
@@ -1,2 +1,1 @@
 import"./MathUtils.js";import{C as t}from"./Color.js";import"./Quaternion.js";import{O as o}from"./Object3D.js";import{L as i}from"./Light.js";function r(r,s,p){i.call(this,r,p),this.type="HemisphereLight",this.castShadow=void 0,this.position.copy(o.DefaultUp),this.updateMatrix(),this.groundColor=new t(s)}r.prototype=Object.assign(Object.create(i.prototype),{constructor:r,isHemisphereLight:!0,copy:function(t){return i.prototype.copy.call(this,t),this.groundColor.copy(t.groundColor),this}});export{r as HemisphereLight};
-//# sourceMappingURL=HemisphereLight.js.map
