8547596c7037f09e357f55b5e4431e14da585554
jascha <jascha@jaeh.at>
2020-10-09 12:43:20 +0200

    docs: update

--- docs/js/loadReticle.js
+++
@@ -1,1 +0,0 @@
-import{RingBufferGeometry as t,MeshBasicMaterial as e,Mesh as o}from"./three.module.js";const r=async()=>{const r=new t(.15,.2,32).rotateX(-Math.PI/2),n=new e,a=new o(r,n);return a.matrixAutoUpdate=!1,a.visible=!1,a};export{r as loadReticle};
